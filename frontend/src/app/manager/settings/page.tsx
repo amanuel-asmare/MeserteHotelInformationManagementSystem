5d07bf5b9b2b0bf18e6d27fe76e2ef1ce2d950f4
amanuel-asmare <amanuelasmare18@gmail.com>
2025-12-20 06:12:13 +0900

    update profile setting

--- frontend/src/app/manager/settings/page.tsx
+++ frontend/src/app/manager/settings/page.tsx
@@ -1,10 +1,12 @@
-// src/app/manager/settings/page.tsx
-import ManagerSettingsClient from './ManagerSettingsClient';
-import ClientWrapper from './ClientWrapper';
-export const metadata = {
-  title: 'Manager Settings | Meseret Hotel',
-};
+import SettingsPage from '@/app/admin/settings/page'
+import React from 'react'
 
-export default function ManagerSettingsPage() {
-  return <ClientWrapper />;
+function ManagerSettings() {
+  return (
+    <div>
+    <SettingsPage/>
+    </div>
+  )
 }
+
+export default ManagerSettings
